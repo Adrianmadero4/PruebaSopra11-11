8487d429fd8570101c11bae007e79f5be48b3457
Adrianmadero4 <adrianmctech@gmail.com>
2024-11-11 19:05:25 +0100

    Ejercicio 2

---
+++ src/Ej2/Persona.java
@@ -0,0 +1,11 @@
+package Ej2;
+
+public class personas {
+    String sexo; // 'M' para masculino, 'F' para femenino
+    int edad;
+
+    public personas(String sexo, int edad) {
+        this.sexo = sexo;
+        this.edad = edad;
+    }
+}
